3b818a16f34035e9198baf85cd630546c6044883
Kewin Titus <kewintitus@gmail.com>
2022-10-10 17:57:35 +0530

    [BUGFIX] - redux bugfixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import './App.css';
 import Feed from './components/body/feed/Feed';
 import Sidebar from './components/body/sidebar/Sidebar';
 import Header from './components/header/Header';
+import { selectUser } from './features/userSlice';
 
 function App() {
+  const user = useSelector(selectUser);
+  console.log(user);
+
   return (
     <div className="app">
       <Header />
--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,16 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-  user: null,
-};
-
 export const userSlice = createSlice({
   name: 'user',
-  initialState,
+  initialState: { user: null },
 
   reducers: {
-    login: (state) => {
-      state.user = true;
+    login: (state, action) => {
+      state.user = action.payload;
     },
     logout: (state) => {
       state.user = false;
@@ -20,6 +16,6 @@ export const userSlice = createSlice({
 
 export const { login, logout } = userSlice.actions;
 
-export const selectUser = (state) => state.user.value;
+export const selectUser = (state) => state.user.user;
 
 export default userSlice.reducer;
